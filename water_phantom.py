c40105023a57eed42e418511e23e2197f9c1b0a3
Shanjie Xiao <xiaosj@users.noreply.github.com>
2020-07-05 15:24:06 -0700

    update array type

--- water_phantom.py
+++ water_phantom.py
@@ -5,5 +5,5 @@ from ct_image import ct_img
 phantom = ct_img()
 phantom.nx = phantom.ny = phantom.nz = 100
 phantom.dx = phantom.dy = phantom.dz = 1.0
-phantom.voxel = np.zeros((phantom.nx, phantom.ny, phantom.nz))
+phantom.voxel = np.zeros((phantom.nx, phantom.ny, phantom.nz), dtype=np.int16)
 phantom.write_img('water_100_1mm.img')
